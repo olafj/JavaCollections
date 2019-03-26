340b0fc9a82797288ed3e322b778fc8d580fdbea
Olaf Jantz <olaf.jantz@appcreation.de>
2019-03-26 10:33:58 +0100

    Java 10

---
+++ src/main/java/tutorial/T7_Java10_Zeugs.java
@@ -0,0 +1,31 @@
+package tutorial;
+
+import java.util.List;
+import java.util.Map;
+
+public class T7_Java10_Zeugs {
+
+    public static void main(String[] args) {
+
+        // Wenn man schnell mal Werte in eine Liste packen muss (z.B. Konstante, statische Liste)
+
+        var benutzerRollen = List.of("user", "admin", "superadmin");
+
+        // oder auch eine Map
+
+        var wochenTage = Map.of("mo", "Montag", "di", "Dienstag", "mi", "Mittwoch");
+        System.out.println(wochenTage.get("mo"));
+
+        // einfachste For-Schleife mit Lamda
+
+        benutzerRollen.forEach(rolle -> {
+            System.out.println(rolle);
+        });
+
+        wochenTage.forEach((key, value) -> {
+            System.out.println("key=" + key + " => value=" + value);
+        });
+
+
+    }
+}
